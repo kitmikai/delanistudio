4f4fb194aa4f12a2296a6a533ad2e0fc0d7c260f
kitmikai <matara.timothy@gmail.com>
2020-06-22 00:17:42 +0300

    Add popup function and form validation

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
-$(document).ready(function() {
-    // What we do box toggle effect
-    $('#desbox').click(function() {
-        $('#desbox').addClass('active');
-        $('.desbox').toggleClass('active');
-        $('#design').toggle();
-        $('#desbox').removeClass('active');
-    });
+$(document).ready(function(){
 
-    $('#devbox').on('click', function () {
-        $('#devbox').addClass('active');
-        $('.devbox').toggleClass('active');
-        $('#dev').toggle();
-        $('#devbox').removeClass('active');
-    });
+    $(function() {
+         // What we do box toggle effect
+        $('#desbox').click(function() {
+            $('#desbox').addClass('active');
+            $('.desbox').toggleClass('active');
+            $('#design').toggle();
+            $('#desbox').removeClass('active');
+        });
 
-    $('#prodbox').on('click', function () {
-        $('#prodbox').addClass('active');
-        $('.prodbox').toggleClass('active');
-        $('#product').toggle();
-        $('#prodbox').removeClass('active');
-    });
+        $('#devbox').on('click', function () {
+            $('#devbox').addClass('active');
+            $('.devbox').toggleClass('active');
+            $('#dev').toggle();
+            $('#devbox').removeClass('active');
+        });
 
-    // Portfolio hover effect
-    const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper')
+        $('#prodbox').on('click', function () {
+            $('#prodbox').addClass('active');
+            $('.prodbox').toggleClass('active');
+            $('#product').toggle();
+            $('#prodbox').removeClass('active');
+        });
+
+        // Portfolio hover effect
+        const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
+
         portfolioItems.forEach(portfolioItem => {
             portfolioItem.addEventListener('mouseover', () => {
                 portfolioItem.childNodes[1].classList.add('img-lighter');
@@ -31,12 +34,89 @@ $(document).ready(function() {
             portfolioItem.addEventListener('mouseout', () => {
                 portfolioItem.childNodes[1].classList.remove('img-lighter');
             });
-    });
+        });
 
-    $('submit').on('click', function(){
-        const name = $('#txtname').val();
-        var strLength = $.trim(name).length;
-        alert('Dear ' + name + ', We have received your message. Thank you for reaching out!');
-    });
+        $("#cname_error_message").hide();
+        $("#email_error_message").hide();
+        $("#msg_error_message").hide();
+
+        var error_cname = false;
+        var error_email = false;
+        var error_message = false;
+       
+        $("#txtname").focusout(function(){
+           check_cname();
+        });
+      
+        $("#txtemail").focusout(function() {
+           check_email();
+        });
+        $("#txtmessage").focusout(function() {
+           check_length();
+        });
+       
     
+        function check_cname() {
+           var pattern = /^[a-zA-Z]*$/;
+           var cname = $("#txtname").val();
+           if (pattern.test(cname) && cname !== '') {
+              $("#cname_error_message").hide();
+              $("#txtname").css("border","2px solid #34F458");
+           } else {
+              $("#cname_error_message").html("Should contain only Characters");
+              $("#cname_error_message").show();
+              $("#txtname").css("border","2px solid #F90A0A");
+              error_cname = true;
+           }
+        }
+    
+        function check_length() {
+            var message_length = $("#txtmessage").val().length;
+            if (message_length < 1) {
+                $("#msg_error_message").html("Please type a message.");
+                $("#msg_error_message").show();
+                $("txtmessage").css("border","2px solid #F90A0A");
+                error_message = true;
+            } else {
+                $("#msg_error_message").hide();
+                $("#txtmessage").css("border","2px solid #34F458");
+            }
+
+        }
+    
+        function check_email() {
+           var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+           var email = $("#txtemail").val();
+           if (pattern.test(email) && email !== '') {
+              $("#email_error_message").hide();
+              $("#txtemail").css("border","2px solid #34F458");
+           } else {
+              $("#email_error_message").html("Invalid Email");
+              $("#email_error_message").show();
+              $("#txtemail").css("border","2px solid #F90A0A");
+              error_email = true;
+           }
+        }
+    
+        $("#contact_form").submit(function() {
+           error_cname = false;
+           error_email = false;
+           error_message = false;
+           cusName = $('#txtname').val();
+    
+           check_cname();
+           check_email();
+           check_length();
+
+           if (error_cname === false && error_email === false && error_message === false) {
+              alert("Dear " + cusName + ", \nWe have received your message.\nThank you for reaching out!");
+              return true;
+           } else {
+              alert("Please Fill the form Correctly");
+              return false;
+           }
+    
+        });
+     });
+
 });
